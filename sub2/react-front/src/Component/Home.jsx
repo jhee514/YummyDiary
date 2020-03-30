4e89ee97ea3348bebdb888c650b8916941c122a1
김한솔 <anf36@naver.com>
2020-03-30 17:59:08 +0900

    200330|create new branch|Hansol|35
    Former-commit-id: de240a25fab57535bc74220e58c9bf5e961d90ed

--- sub2/react-front/src/Component/Home.jsx
+++ sub2/react-front/src/Component/Home.jsx
@@ -1,13 +1,13 @@
 import React from "react"
-import {Typography} from "@material-ui/core"
+import {Typography, Box} from "@material-ui/core"
 
 
 const Home = props => {
 
   return(
-    <>
+    <Box>
       <Typography>Home Content</Typography>
-    </>
+    </Box>
   )
 }
 export default Home
