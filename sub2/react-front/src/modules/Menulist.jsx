7ed499130528989c2a87353f25083360fb9b7cdc
김한솔 <anf36@naver.com>
2020-04-02 13:26:43 +0900

    200402 | update Menulist.jsx & other components | Hansol | S02P22A103-35
    Former-commit-id: b974827c91aefcac381d8e7b4188e962e37239a5

---
+++ sub2/react-front/src/modules/Menulist.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+const Menulist = props => {
+  const menumap = {
+    "/": [
+      { id : 1,menuname: "home", menupath: "/" },
+      { id : 2, menuname: "signup", menupath: "/signup" }
+    ],
+    "/signup": [{ id:1 ,menuname: "home", menupath: "/" }]
+  };
+  const currentHistory = props.currentHistory;
+  console.log(currentHistory)
+  return (
+    <>
+      {menumap[currentHistory].map(menu => (
+        <Link to={menu.menupath}  key={menu.id}>
+          <Button>{menu.menuname}</Button>
+        </Link>
+      ))}
+    </>
+  );
+};
+export default Menulist
