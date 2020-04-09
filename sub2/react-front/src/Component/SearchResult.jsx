d8e2a0e91a0b09d42c12dd09daa47c16e364870f
akstlrdlsms <59681229+akstlrdlsms@users.noreply.github.com>
2020-04-09 15:06:08 +0900

    200409 | add searchResult component | Minsik
    Yeah!
    
    
    Former-commit-id: efabce24c9360e2968728454c1ad99c6cc425163

--- sub2/react-front/src/Component/SearchResult.jsx
+++ sub2/react-front/src/Component/SearchResult.jsx
@@ -7,13 +7,18 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
 import { Box } from '@material-ui/core';
+import {storedata, hashdata} from "../modules/dummy";
 
 const useStyles = makeStyles({
-  root: {
-    
+  storeSearchResult_root: {
     minWidth: 275,
-    width:"50%",
-    marginTop: "100px"
+    width:"25%",
+    marginTop: "15px"
+  },
+  hashtagSearchResult_root:{
+    minWidth: 275,
+    width:"25%",
+    marginTop: "15px"
   },
   bullet: {
     display: 'inline-block',
@@ -26,11 +31,30 @@ const useStyles = makeStyles({
   pos: {
     marginBottom: 12,
   },
-  searchCard_box: {
-      backgroundColor: "rgb(255,0,0)",
+  storeSearchResult_Box: {
+    marginTop: "30px",
+  },
+  hashtagSearchResult_Box: {
+    marginTop: "70px",
+  },
+  storeSearchResult_Card_Box: {
+      backgroundColor: "rgb(255,255,204)",
       display:"flex",
       justifyContent:"center"
   },
+  storeSearchResult: {
+    fontSize: "40px",
+    textAlign: "center"
+  },
+  hashtagSearchResult_Card_Box: {
+    backgroundColor: "rgb(255,255,204)",
+    display:"flex",
+    justifyContent:"center"
+  },
+  hashtagSearchResult: {
+    fontSize: "40px",
+    textAlign: "center"
+  }
 });
 
 export default function OutlinedCard() {
@@ -38,28 +62,70 @@ export default function OutlinedCard() {
   const bull = <span className={classes.bullet}>•</span>;
 
   return (
-    <Box className={classes.searchCard_box}>
-        <Card className={classes.root} variant="outlined">
+    <Box className={classes.searchResult_layout}>
+      <Box className={classes.storeSearchResult_Box}>
+        <Typography className={classes.storeSearchResult}>
+          This is "Store" Search Result
+        </Typography>
+      </Box>
+      
+      <Box className={classes.storeSearchResult_Card_Box}>
+        {storedata.map((data) => (
+          <Card className={classes.storeSearchResult_root} variant="outlined">
             <CardContent>
                 <Typography className={classes.title} color="textSecondary" gutterBottom>
-                Word of the Day
+                  {data.id}
                 </Typography>
                 <Typography variant="h5" component="h2">
-                be{bull}nev{bull}o{bull}lent
+                  {data.storeName}
                 </Typography>
                 <Typography className={classes.pos} color="textSecondary">
-                adjective
+                  {data.content}
                 </Typography>
                 <Typography variant="body2" component="p">
-                well meaning and kindly.
-                <br />
-                {'"a benevolent smile"'}
+                  {data.rating}점
+                <br/><br/>
+                  리뷰:{data.review_cnt}개
                 </Typography>
             </CardContent>
             <CardActions>
-                <Button size="small">Learn More</Button>
+                <Button size="small">더 보기</Button>
             </CardActions>
-        </Card>
+          </Card>
+      ))}
+      </Box>
+
+      <Box className={classes.hashtagSearchResult_Box}>
+        <Typography className={classes.hashtagSearchResult}>
+          This is "Hash Tag" Search Result
+        </Typography>
+      </Box>
+      
+      <Box className={classes.hashtagSearchResult_Card_Box}>
+        {hashdata.map((data) => (
+          <Card className={classes.hashtagSearchResult_root} variant="outlined">
+            <CardContent>
+                  <Typography className={classes.title} color="textSecondary" gutterBottom>
+                    {data.id}
+                  </Typography>
+                  <Typography variant="h5" component="h2">
+                    {data.storeName}
+                  </Typography>
+                  <Typography className={classes.pos} color="textSecondary">
+                    {data.content}
+                  </Typography>
+                  <Typography variant="body2" component="p">
+                    {data.rating}점
+                  <br/><br/>
+                    리뷰:{data.review_cnt}개
+                  </Typography>
+              </CardContent>
+              <CardActions>
+                  <Button size="small">더 보기</Button>
+              </CardActions>
+          </Card>
+        ))}
+      </Box>
     </Box>
   );
 }
