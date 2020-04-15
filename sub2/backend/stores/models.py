546251eeada30f2b54698769612b89ed8a060762
Kim, Ji Hee <514kim@gmail.com>
2020-04-15 23:10:39 +0900

    041520 | json data to csv, update pip requirement | Jihee
    Former-commit-id: b0b469c9e258c9f4bd4466a953312cd5ef80658a [formerly 5eea0adcb7b840425692b986862762ba7673136f]
    Former-commit-id: ac0414c60837c115ecd56c31322ad55edb0ea74a

--- sub2/backend/stores/models.py
+++ sub2/backend/stores/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Store(models.Model):
-    store_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     branch = models.CharField(max_length=100, null=True)
     area = models.CharField(max_length=100, null=True)
     tel = models.CharField(max_length=30, null=True)
