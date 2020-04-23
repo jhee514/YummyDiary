b8802a43edcffd6da1765e2b4462884df9c97b64
anfelal35 <anf36@naver.com>
2020-04-23 23:54:43 +0900

    200423 | update detail with fetch data | Hansol
    Former-commit-id: d50d9a59fa0d6a5aff20164fd481661f67981c18

--- YUMMY_DIARY/frontend/src/Component/Detail.jsx
+++ YUMMY_DIARY/frontend/src/Component/Detail.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Box,
   makeStyles,
@@ -8,11 +8,10 @@ import {
 } from "@material-ui/core";
 import { storedetail } from "../modules/dummy";
 import { Rating } from "@material-ui/lab";
-import { useState } from "react";
 import KakaoMap from "./KakaoMap";
 import { usePosition } from "use-position";
-import {getDistance} from "../modules/getDistance"
-
+import { getDistance } from "../modules/getDistance";
+import axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -51,84 +50,127 @@ const useStyles = makeStyles((theme) => ({
   },
   divider: {},
 }));
-const Detail = (porps) => {
+const Detail = (props) => {
   const classes = useStyles();
   const [unfold, setUnfold] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [store, setStore] = useState({});
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setStore({});
+        setLoading(true);
+        const response = await axios.get(
+          "http://localhost:8000/stores/stores/" + props.match.params.id
+        );
+        setStore(response.data);
+      } catch (e) {
+        console.error(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
   const foldEvent = (event) => {
     setUnfold(!unfold);
   };
+  // console.log(props.match.params)
   const watch = true;
   const { latitude, longitude, timestamp, accuracy, error } = usePosition(
     watch
   );
 
   return (
-    <Box className={classes.root}>
-      <Box className={classes.mapbox}>
-        <Box className={classes.contentbox}>
-          {/* <img src="/dummymap.png" alt="map" className={classes.map} /> */}
-          {
-            latitude === undefined ?  <></>:
-          <KakaoMap target_lang={37.5718251} target_long={126.9803754} current_lang = {latitude} current_long={longitude}/>
-          }
-          <Typography variant="h6">
-            <a
-              href={
-                "https://map.kakao.com/link/to/" +
-                "오목집,37.5718251,126.9803754"
-              }
-            >
-              길찾기
-            </a>
-          </Typography>
-            <Typography>거리 : {getDistance(37.5718251,126.9803754,latitude,longitude)} KM</Typography>
-        </Box>
-        {/* 37.5718251 , 126.9803754 */}
-      </Box>
-      <Box className={classes.storedetail}>
-        <Box className={classes.contentbox}>
-          <Box className={classes.content}>
-            <Typography variant="h4">
-              {storedetail.storeName}
-              <Rating value={storedetail.rating} disabled precision="0.1" />
-            </Typography>
-            <Typography variant="h6">
-              {storedetail.tags.map((tag, index) => tag + " ")}
-            </Typography>
-          </Box>
-          <Divider variant="middle" />
-          <Box className={classes.content}>
-            <Typography variant="h6">영업 시간</Typography>
-            {storedetail.open.map((time, index) => (
-              <Typography variant="body2" key={index}>
-                {time}
-              </Typography>
-            ))}
-          </Box>
-          <Divider variant="middle" />
-          <Box className={classes.content}>
-            {storedetail.menuList.map((menu, index) =>
-              (unfold ? 2 : storedetail.menuList.length) >= index ? (
-                <Box display="flex" justifyContent="space-between">
-                  <Typography>{menu.menuName}</Typography>
-                  <Typography>{menu.menuPrice}원</Typography>
-                </Box>
-              ) : (
+    <>
+      {loading ? (
+        <></>
+      ) : (
+        <Box className={classes.root}>
+          <Box className={classes.mapbox}>
+            <Box className={classes.contentbox}>
+              {/* <img src="/dummymap.png" alt="map" className={classes.map} /> */}
+              {latitude === undefined ? (
                 <></>
-              )
-            )}
-            <Box
-              display="flex"
-              justifyContent="flex-end"
-              marginTop={1}
-              onClick={foldEvent}
-            >
-              <Button variant="text">{unfold ? "더보기" : "접기"}</Button>
+              ) : (
+                <KakaoMap
+                  target_lang={store.latitude}
+                  target_long={store.longitude}
+                  current_lang={latitude}
+                  current_long={longitude}
+                />
+              )}
+              <Typography variant="h6">
+                <a
+                  href={
+                    "https://map.kakao.com/link/to/" +
+                    "오목집,37.5718251,126.9803754"
+                  }
+                >
+                  길찾기
+                </a>
+              </Typography>
+              <Typography>
+                거리 :{" "}
+                {getDistance(
+                  store.latitude,
+                  store.longitude,
+                  latitude,
+                  longitude
+                )}{" "}
+                KM
+              </Typography>
+            </Box>
+            {/* 37.5718251 , 126.9803754 */}
+          </Box>
+          <Box className={classes.storedetail}>
+            <Box className={classes.contentbox}>
+              <Box className={classes.content}>
+                <Typography variant="h4">
+                  {store.name}
+                  <Rating value={storedetail.rating} disabled precision="0.1" />
+                </Typography>
+                <Typography variant="h6">
+                  {/* {storedetail.tags.map((tag, index) => tag + " ")} */}
+                  {store.category}
+                </Typography>
+                <Typography variant="h6">{store.address}</Typography>
+              </Box>
+              <Divider variant="middle" />
+              <Box className={classes.content}>
+                <Typography variant="h6">영업 시간</Typography>
+                {storedetail.open.map((time, index) => (
+                  <Typography variant="body2" key={index}>
+                    {time}
+                  </Typography>
+                ))}
+              </Box>
+              <Divider variant="middle" />
+              <Box className={classes.content}>
+                {storedetail.menuList.map((menu, index) =>
+                  (unfold ? 2 : storedetail.menuList.length) >= index ? (
+                    <Box display="flex" justifyContent="space-between">
+                      <Typography>{menu.menuName}</Typography>
+                      <Typography>{menu.menuPrice}원</Typography>
+                    </Box>
+                  ) : (
+                    <></>
+                  )
+                )}
+                <Box
+                  display="flex"
+                  justifyContent="flex-end"
+                  marginTop={1}
+                  onClick={foldEvent}
+                >
+                  <Button variant="text">{unfold ? "더보기" : "접기"}</Button>
+                </Box>
+              </Box>
             </Box>
           </Box>
         </Box>
-      </Box>
-    </Box>
+      )}
+    </>
   );
 };
 
