ddb03cb51a27f0125c30a3811b6855ceb2a7010d
anfelal35 <anf36@naver.com>
2020-04-24 16:29:37 +0900

    200424 | update store model & serializers for detail page | Hansol
    Former-commit-id: 8eb6d29140a0674c726c24dc8e471468543c5749

--- YUMMY_DIARY/backend/stores/serializers.py
+++ YUMMY_DIARY/backend/stores/serializers.py
@@ -8,8 +8,31 @@ class TagSerializer(serializers.ModelSerializer):
             "content",
         ]
 
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = [
+            "name",
+            "price",
+            "store",
+        ]
+
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = [
+            "writer",
+            "store",
+            "total_score",
+            "content",
+            "reg_time",
+        ]
 
 class StoreSerializer(serializers.ModelSerializer):
+    review = ReviewSerializer(many=True, read_only=True)
+    menu = MenuSerializer(many=True, read_only=True)
     class Meta:
         model = Store
         fields = [
@@ -24,26 +47,7 @@ class StoreSerializer(serializers.ModelSerializer):
             "bhour",
             "image",
             "tags",
+            "review",
+            "menu",
         ]
 
-
-class ReviewSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Review
-        fields = [
-            "writer",
-            "store",
-            "total_score",
-            "content",
-            "reg_time",
-        ]
-
-
-class MenuSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Menu
-        fields = [
-            "name",
-            "price",
-            "store",
-        ]
