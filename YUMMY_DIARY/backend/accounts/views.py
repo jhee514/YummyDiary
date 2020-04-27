d954ca1544ea925ddbffdbf10b00d15fa6b57d3f
오동현 <dhehdgus512@naver.com>
2020-04-27 20:12:32 +0900

    200427|modify accounts serializer|Hansol
    Former-commit-id: 17825f3fd21c7e095876f8134a7c9e05e42a1138

--- YUMMY_DIARY/backend/accounts/views.py
+++ YUMMY_DIARY/backend/accounts/views.py
@@ -37,9 +37,12 @@ def user_page(request):
     if request.method == 'PATCH':
         serializer = UserSerializer(instance=user, data=request.data, partial=True)
         if serializer.is_valid():
-            new_user = serializer.save()
-            new_user.set_password(request.data["password"])
-            new_user.save()
+            if 'password' in request.data:
+                new_user = serializer.save()
+                new_user.set_password(request.data["password"])
+                new_user.save()
+            else:
+                serializer.save()
             return Response(serializer.data)
         return Response(status=400, data=serializer.errors)
 
