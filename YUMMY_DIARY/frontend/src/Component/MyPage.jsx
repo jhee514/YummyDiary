d1d760a66287845280d730116472d5de3aa68c96
pms <akstlrdlsms@naver.com>
2020-05-01 03:11:52 +0900

    Merge branch 'front_mypageDummy' of https://lab.ssafy.com/s02-bigdata-sub3/s02p23a103 into front_mypageDummy
    ggg
    
    
    Former-commit-id: 3e612414e1e6b9b85d57d1decfcd7e0d507a1123

--- YUMMY_DIARY/frontend/src/Component/MyPage.jsx
+++ YUMMY_DIARY/frontend/src/Component/MyPage.jsx
@@ -1,140 +1,105 @@
-import React from "react";
-import {makeStyles, Container, Avatar, Grid, Paper} from '@material-ui/core';
+import React, { useEffect, useState } from "react";
+import { makeStyles, Container,Typography, Avatar, Grid, Box,Paper } from "@material-ui/core";
+import axios from "axios";
+import MyPageScore from "./MyPage/MyPageScore";
+import Slider from "react-slick";
 
-
-const useStyles = makeStyles(theme => ({
-    container: {
-      margin: "20px 15px",
+const useStyles = makeStyles((theme) => ({
+    recommendbox:{
+        marginTop: "8vh",
+        //border: "1px solid #FAC60E",
+        width: "70%",
+        minWidth: "80vw",
+        flexDirection: "column",
+        alignItems: "center",
     },
-    title: {
-        fontSize: "30px"
+    mypage_div: {
+        alignItems: "center",
     },
-    content: {
-      width: "100%",
-      height: "100%",
-      fontSize: "20px"
+    root: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        minHeight: "600px",
     },
-    item: {
-        width: "20vw",
-        height: "300px",
-        padding: "20px",
-        fontSizd: "15px",
-        textAlign: "center"
+    hr_tag: {
+        width: "98%",
+        border: "solid 2px",
+        borderColor: "#FAC60E"
     }
-  }));
+}));
 
 const MyPage = (props) => {
-    const classes = useStyles();
+  const settings = {
+    // infinite: posts.length > 3 ?true:false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  };
 
-    return (
-        <Container className={classes.container}>
-            {/* 프로필 및 식단 작성 부분 */}
-            <h1 className={classes.title}>
-                My page
-            </h1>
-            <Grid className={classes.container} container justify="space-between">
-                <Grid item spacing={4}>
-                    <p className={classes.content}>
-                        <Avatar></Avatar> user id
-                    </p>
-                </Grid>
-                <Grid item container justify="space-around" alignItems="center">
-                    <Grid item spacing={2}>
-                        <Paper className={classes.item}>
-                            <h2>Breakfast</h2>
-                            <hr/>
-                            <p>Date: 2020 April 9th 10:00 am</p>
-                            <p>Location: Home</p>
-                            <p>Dish: Spagetti</p>
-                            <p>Rating: ★★★★☆</p>
-                        </Paper>
-                    </Grid>
-                    <Grid item spacing={2}>
-                        <Paper className={classes.item}>
-                            <h2>Lunch</h2>
-                            <hr/>
-                            <p>Date: 2020 April 9th 1:00 pm</p>
-                            <p>Location: 맛나분식</p>
-                            <p>Dish: 떡볶이</p>
-                            <p>With: 도희</p>
-                            <p>Rating: ★★★☆☆</p>
-                        </Paper>
-                    </Grid>
-                    <Grid item spacing={2}>
-                        <Paper className={classes.item}>
-                            <h2>Dinner</h2>
-                            <hr/>
-                            <p>Date: 2020 April 9th 6:00 pm</p>
-                            <p>Location: 회기 Eggdrop</p>
-                            <p>Dish: 토스트랑 커피</p>
-                            <p>With: 지희</p>
-                            <p>Rating: ★★★★★</p>
-                        </Paper>
-                    </Grid>
-                    <Grid item spacing={2}>
-                        <Paper className={classes.item}>
-                            <h2>+</h2>
-                        </Paper>
-                    </Grid>
-                </Grid>
-            </Grid>
+  const classes = useStyles();
+  const [posts, setPosts] = useState([]);
 
-            <hr/>
-            
-            {/* 다녀온 맛집 리스트(참여한 리뷰) */}
-            <h1 className={classes.title}>
-                Visited Place
-            </h1>
-            <Grid className={classes.container} container justify="space-between">
-                <Grid item container justify="space-around" alignItems="center">
-                    <Grid item spacing={2}>
-                        <Paper className={classes.item}>
-                            <h2>도미노피자</h2>
-                            <hr/>
-                            <p>Date: 2020 March 9th 10:00 am</p>
-                            <p>Location: 서울시 동대문구 회기로 9가길</p>
-                            <p>Dish: 포테이토피자 M</p>
-                            <p>With: 동현, 민식, 한솔</p> 
-                            <p>Rating: ★★★★☆</p>
-                        </Paper>
-                    </Grid>
-                    <Grid item spacing={2}>
-                        <Paper className={classes.item}>
-                            <h2>맛나분식</h2>
-                            <hr/>
-                            <p>Date: 2020 April 9th 1:00 pm</p>
-                            <p>Location: 동대문구 휘경로 4길 29</p>
-                            <p>Dish: 떡볶이</p>
-                            <p>With: 도희</p>
-                            <p>Rating: ★★★☆☆</p>
-                        </Paper>
-                    </Grid>
-                    <Grid item spacing={2}>
-                        <Paper className={classes.item}>
-                            <h2>Eggdrop</h2>
-                            <hr/>
-                            <p>Date: 2020 April 9th 6:00 pm</p>
-                            <p>Location: Eggdrop 경희대점</p>
-                            <p>Dish: 토스트랑 커피</p>
-                            <p>With: 지희</p>
-                            <p>Rating: ★★★★★</p>
-                        </Paper>
-                    </Grid>
-                    <Grid item spacing={2}>
-                        <Paper className={classes.item}>
-                            <h2>다정식당</h2>
-                            <hr/>
-                            <p>Date: 2020 April 9th 8:00 pm</p>
-                            <p>Location: 서울시 동대문구 이문동 283-6</p>
-                            <p>Dish: 제육볶음, 맥주</p>
-                            <p>With: 도희, 루이</p>
-                            <p>Rating: ★★★★★</p>
-                        </Paper>
-                    </Grid>
-                </Grid>
-            </Grid>
-        </Container>
-    );
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        // 배포할때 아래 주석 풀어주어야함...
+        //   url + "/accounts/myposts/",
+        //   { headers: { authorization: "jwt " + sessionStorage.getItem("token") } }
+
+        // 로컬환경은 아래 주석 풀어주어야함...
+        "http://localhost:8000/api/accounts/myposts/",
+        { headers: { authorization: "jwt " + sessionStorage.getItem("token") } }
+      );
+      console.log(response.data);
+      setPosts(response.data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <Box className={classes.root}>
+      {/* 프로필 및 식단 작성 부분 */}
+      <h1 className={classes.title}>My DIARY</h1>
+      <Box className={classes.recommendbox}>
+        <Slider {...settings}>
+            {posts.map((post, index) => (
+              //   <Grid item spacing={2}>
+              //     <Paper className={classes.item}>
+              <div key={post.pk} className={classes.mypage_div}>
+                {post.fields.meal == 1 ? (
+                  <Typography variant="h5">Breakfast</Typography>
+                ) : post.fields.meal == 2 ? (
+                  <Typography variant="h5">Lunch</Typography>
+                ) : (
+                  <Typography variant="h5">Dinner</Typography>
+                )}
+                <hr className={classes.hr_tag} />
+                <Typography variant="h6">
+                  Date:{" "}
+                  {post.fields.created_at.substring(0, 4) +
+                    "년 " +
+                    post.fields.created_at.substring(5, 7) +
+                    "월 " +
+                    post.fields.created_at.substring(8, 10) +
+                    "일"}
+                </Typography>
+                <Typography variant="h6">Location: {post.fields.location}</Typography>
+                  <Typography variant="h6">함께한 사람: {post.fields.party}</Typography>
+                <Typography variant="h6">Dish: {post.fields.menus}</Typography>
+                <Typography variant="h6">
+                  Rating: {post.fields.score.toFixed(1.0)}
+                  {/* <MyPageScore score={post.fields.score} /> */}
+                </Typography>
+              </div>
+              //     </Paper>
+              //   </Grid>
+            ))}
+        </Slider>
+        </Box>
+    </Box>
+  );
 };
 
 export default MyPage;
